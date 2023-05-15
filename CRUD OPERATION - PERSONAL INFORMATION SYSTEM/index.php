816554abaddabd936b121d7753f8d3920fd6fdbe
Mark Dela Paz <markanghelod@gmail.com>
2023-05-15 14:56:15 +0800

    All

--- CRUD OPERATION - PERSONAL INFORMATION SYSTEM/index.php
+++ CRUD OPERATION - PERSONAL INFORMATION SYSTEM/index.php
@@ -10,7 +10,7 @@
 * {
   	box-sizing: border-box;
   	font-family: -apple-system, BlinkMacSystemFont, "segoe ui", roboto, oxygen, ubuntu, cantarell, "fira sans", "droid sans", "helvetica neue", Arial, sans-serif;
-  	font-size: 17px;
+  	font-size: 16px;
   	-webkit-font-smoothing: antialiased;
   	-moz-osx-font-smoothing: grayscale;
 }
