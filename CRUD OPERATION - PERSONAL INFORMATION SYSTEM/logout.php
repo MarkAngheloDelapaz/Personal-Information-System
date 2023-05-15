97c8df5289dff6d07ac0b8c4e7847556bcada390
Mark Dela Paz <markanghelod@gmail.com>
2023-05-15 15:02:04 +0800

    Revert "CRUD OPERATION FOR PERSONAL INFO SYSTEM"
    This reverts commit 4ec29a7f0b3be7fd53a1ec1efe298802f5763df6.

--- CRUD OPERATION - PERSONAL INFORMATION SYSTEM/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'database.php';
-session_start();
-$conn = DBconnection();
-$conn->query("update users set active ='0' where user_id= '".$_GET["id"]."'"); //set 0 as in active status of user
-session_destroy();
-// Redirect to the login page:
-header('Location: index.php');
-?>
