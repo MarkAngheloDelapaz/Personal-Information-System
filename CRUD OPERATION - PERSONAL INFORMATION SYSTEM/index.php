994d518a0a65f7f6586c477b2824be4af0722343
Mark Dela Paz <markanghelod@gmail.com>
2023-05-15 16:03:39 +0800

    additional font smoothing

--- CRUD OPERATION - PERSONAL INFORMATION SYSTEM/index.php
+++ CRUD OPERATION - PERSONAL INFORMATION SYSTEM/index.php
@@ -13,6 +13,7 @@
   	font-size: 18px;
   	-webkit-font-smoothing: antialiased;
   	-moz-osx-font-smoothing: grayscale;
+	-moz-osx-font-smoothing: grayscale;
 }
 body {
   	background-color: #435165;
